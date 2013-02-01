8ede76af83146c9b39b31864765820ddd61783b5
Rubén <ruben.vanyo@gmail.com>
2013-02-01 14:02:12 +0100

    muestra en listview los datos de la agend

---
+++ EjemploContentProvider/src/org/ejemplo/aguilar/EjemploContentProvider.java
@@ -0,0 +1,13 @@
+package org.ejemplo.aguilar;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class EjemploContentProvider extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
